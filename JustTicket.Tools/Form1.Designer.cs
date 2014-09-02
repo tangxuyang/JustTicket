b725e5be802db81952d5f183d8f48f76056777b8
txy <tangxuyang.hi@163.com>
2014-09-02 22:03:48 +0800

    Add DTOHelper

---
+++ JustTicket.Tools/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace JustTicket.Tools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_Main = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btn_GetCityList = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tb_CityFilter = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_Date = new System.Windows.Forms.TextBox();
+            this.tb_From = new System.Windows.Forms.TextBox();
+            this.tb_To = new System.Windows.Forms.TextBox();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tb_TrainFilter = new System.Windows.Forms.TextBox();
+            this.tb_Main.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_Main
+            // 
+            this.tb_Main.Controls.Add(this.tabPage1);
+            this.tb_Main.Controls.Add(this.tabPage2);
+            this.tb_Main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb_Main.Location = new System.Drawing.Point(0, 0);
+            this.tb_Main.Name = "tb_Main";
+            this.tb_Main.SelectedIndex = 0;
+            this.tb_Main.Size = new System.Drawing.Size(626, 594);
+            this.tb_Main.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tb_CityFilter);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.btn_GetCityList);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(618, 568);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "City";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.tb_TrainFilter);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Controls.Add(this.btn_Search);
+            this.tabPage2.Controls.Add(this.tb_To);
+            this.tabPage2.Controls.Add(this.tb_From);
+            this.tabPage2.Controls.Add(this.tb_Date);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(618, 568);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Trains";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btn_GetCityList
+            // 
+            this.btn_GetCityList.Location = new System.Drawing.Point(504, 20);
+            this.btn_GetCityList.Name = "btn_GetCityList";
+            this.btn_GetCityList.Size = new System.Drawing.Size(86, 23);
+            this.btn_GetCityList.TabIndex = 0;
+            this.btn_GetCityList.Text = "GetCityList";
+            this.btn_GetCityList.UseVisualStyleBackColor = true;
+            this.btn_GetCityList.Click += new System.EventHandler(this.btn_GetCityList_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 101);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(612, 464);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // tb_CityFilter
+            // 
+            this.tb_CityFilter.Location = new System.Drawing.Point(490, 74);
+            this.tb_CityFilter.Name = "tb_CityFilter";
+            this.tb_CityFilter.Size = new System.Drawing.Size(100, 21);
+            this.tb_CityFilter.TabIndex = 2;
+            this.tb_CityFilter.TextChanged += new System.EventHandler(this.tb_CityFilter_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(186, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "From";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(353, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "To";
+            // 
+            // tb_Date
+            // 
+            this.tb_Date.Location = new System.Drawing.Point(80, 29);
+            this.tb_Date.Name = "tb_Date";
+            this.tb_Date.Size = new System.Drawing.Size(100, 21);
+            this.tb_Date.TabIndex = 1;
+            // 
+            // tb_From
+            // 
+            this.tb_From.Location = new System.Drawing.Point(233, 30);
+            this.tb_From.Name = "tb_From";
+            this.tb_From.Size = new System.Drawing.Size(100, 21);
+            this.tb_From.TabIndex = 1;
+            // 
+            // tb_To
+            // 
+            this.tb_To.Location = new System.Drawing.Point(400, 29);
+            this.tb_To.Name = "tb_To";
+            this.tb_To.Size = new System.Drawing.Size(100, 21);
+            this.tb_To.TabIndex = 1;
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Location = new System.Drawing.Point(400, 95);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(75, 23);
+            this.btn_Search.TabIndex = 2;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 159);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 23;
+            this.dataGridView2.Size = new System.Drawing.Size(612, 406);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // tb_TrainFilter
+            // 
+            this.tb_TrainFilter.Location = new System.Drawing.Point(80, 95);
+            this.tb_TrainFilter.Name = "tb_TrainFilter";
+            this.tb_TrainFilter.Size = new System.Drawing.Size(100, 21);
+            this.tb_TrainFilter.TabIndex = 4;
+            this.tb_TrainFilter.TextChanged += new System.EventHandler(this.tb_TrainSearch_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 594);
+            this.Controls.Add(this.tb_Main);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tb_Main.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tb_Main;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_GetCityList;
+        private System.Windows.Forms.TextBox tb_CityFilter;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.TextBox tb_To;
+        private System.Windows.Forms.TextBox tb_From;
+        private System.Windows.Forms.TextBox tb_Date;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_TrainFilter;
+    }
+}
+
---
+++ JustTicket.Tools/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JustTicket.Engining;
+using System.IO;
+
+namespace JustTicket.Tools
+{
+    public partial class Form1 : Form
+    {
+        Engine2 engine;
+        public Form1()
+        {
+            InitializeComponent();
+            engine = new Engine2();
+        }
+
+        private void btn_GetCityList_Click(object sender, EventArgs e)
+        {
+            string fileName = "city.txt";
+            if(File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+            string str = "<actions><DownFile><Method>Get</Method><FileName>city.txt</FileName><Url>https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.806</Url></DownFile></actions>";
+            engine.Execute(str);
+
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            string content = sr.ReadToEnd();
+            sr.Close();
+            content = content.Split('\'')[1];
+
+            string[] strs = content.Split('@');
+            DataTable dt = new DataTable();
+            dt.Columns.Add("CityName");
+            dt.Columns.Add("CityCode");
+            DataRow row = null;
+            foreach(var v in strs)
+            {
+                if (string.IsNullOrEmpty(v))
+                    continue;
+
+                string[] ss = v.Split('|');
+                row = dt.NewRow();
+                row["CityName"] = ss[1];
+                row["CityCode"] = ss[2];
+
+                dt.Rows.Add(row);
+            }
+
+            BindingSource source = new BindingSource();
+            source.DataSource = dt;
+            dataGridView1.DataSource = source;
+            
+        }
+
+        private void tb_CityFilter_TextChanged(object sender, EventArgs e)
+        {
+            BindingSource source = dataGridView1.DataSource as BindingSource;
+            if (source == null)
+                return;
+
+            string filterString = tb_CityFilter.Text;
+
+
+            source.Filter = string.Format("CityName like '%{0}%' or CityCode like '%{0}%'", filterString);
+        }
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            string fileName = "trains.txt";
+            if(File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+            string date = tb_Date.Text;
+            string from = tb_From.Text;
+            string to = tb_To.Text;
+            ///otn/leftTicket/queryT?leftTicketDTO.train_date=2014-09-02&leftTicketDTO.from_station=SHH&leftTicketDTO.to_station=SQF&purpose_codes=ADULT
+            string url = string.Format("https://kyfw.12306.cn/otn/leftTicket/queryT?leftTicketDTO.train_date={0}&amp;leftTicketDTO.from_station={1}&amp;leftTicketDTO.to_station={2}&amp;purpose_codes=ADULT",date,from,to);
+
+            string action = "<actions><DownFile><Method>Get</Method><FileName>trains.txt</FileName><Url>"+url+"</Url></DownFile></actions>";
+
+            Engine2 engine = new Engine2();
+            engine.Execute(action);
+
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            string trains = sr.ReadToEnd();
+            sr.Close();
+            //queryLeftNewDTO
+
+            //int index = 0;
+            //int left, right, length;
+            //string queryLeftNewDTOStr;
+            DataTable dt = new DataTable();
+            dt.Columns.Add("车次");
+            dt.Columns.Add("出发站_到达站");
+            dt.Columns.Add("出发时间");
+            dt.Columns.Add("历时");
+            dt.Columns.Add("商务座");
+            dt.Columns.Add("特等座");
+            dt.Columns.Add("一等座");
+            dt.Columns.Add("二等座");
+            dt.Columns.Add("高级软卧");
+            dt.Columns.Add("软卧");
+            dt.Columns.Add("硬卧");
+            dt.Columns.Add("软座");
+            dt.Columns.Add("硬座");
+            dt.Columns.Add("无座");
+            dt.Columns.Add("其他");
+            
+            //while((index = trains.IndexOf("queryLeftNewDTO",index))>-1)
+            //{
+            //    left = trains.IndexOf("{", index);
+            //    right = trains.IndexOf("}", index) - 1;
+            //    length = right - left;
+            //    queryLeftNewDTOStr = trains.Substring(left + 1, length);
+            List<QueryLeftNewDTO> dtos = DTOHelper.GetDTOPartialInArrayItem<QueryLeftNewDTO>(trains, "queryLeftNewDTO");
+            foreach (var dto in dtos)
+            {
+                //QueryLeftNewDTO dto = new QueryLeftNewDTO(queryLeftNewDTOStr);
+                DataRow row = dt.NewRow();
+                row["车次"] = dto.Station_Train_Code;
+                row["出发站_到达站"] = dto.From_Station_Name + "-" + dto.To_Station_Name;
+                row["出发时间"] = dto.Start_Time;
+                row["历时"] = dto.LiShi;
+                row["商务座"] = dto.SWZ_Num;
+                row["特等座"] = dto.TZ_Num;
+                row["一等座"] = dto.ZY_Num;
+                row["二等座"] = dto.ZE_Num;
+                row["高级软卧"] = dto.GG_Num;
+                row["软卧"] = dto.RW_Num;
+                row["硬卧"] = dto.YW_Num;
+                row["软座"] = dto.RZ_Num;
+                row["硬座"] = dto.YZ_Num;
+                row["无座"] = dto.WZ_Num;
+                row["其他"] = dto.QT_Num;
+                dt.Rows.Add(row);
+            }
+            //    index = right;
+            //}
+            BindingSource bs = new BindingSource();
+            bs.DataSource = dt;
+            dataGridView2.DataSource = bs;
+
+        }
+
+        private void tb_TrainSearch_TextChanged(object sender, EventArgs e)
+        {
+            string trainNo = tb_TrainFilter.Text;
+            string filter = "车次 like '%" + trainNo + "%'";
+            BindingSource bs = dataGridView2.DataSource as BindingSource;
+            bs.Filter = filter;
+        }
+    }    
+}
